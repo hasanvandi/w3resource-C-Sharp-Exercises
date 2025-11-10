4694d062808b31615d4e6d1f979fc9ea488ab36a
hasanvandi <mahdihasanvandi2540@gmail.com>
2025-11-10 12:34:55 +0330

    added swapTwoNumbers

---
+++ SwapTwoNumbers/SwapTwoNumbers/Program.cs
@@ -0,0 +1,17 @@
+﻿int number_01, number_02, temp;
+
+
+Console.Write("Input the First Number:");
+number_01 =Convert.ToInt32(Console.ReadLine());
+Console.Write("Input the Second Number:");
+number_02 = Convert.ToInt32(Console.ReadLine());
+
+
+temp = number_01;
+number_01 = number_02;
+number_02 = temp;
+
+Console.WriteLine("First Number:" + number_01);
+Console.WriteLine("Second  Number:" + temp);
+
+Console.ReadLine();
