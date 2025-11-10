2101925825456644aa8f68ad7aefe5bed10b56b1
hasanvandi <mahdihasanvandi2540@gmail.com>
2025-11-10 12:58:51 +0330

    ArithmeticOperations added

---
+++ ArithmeticOperations/ArithmeticOperations/Program.cs
@@ -0,0 +1,15 @@
+﻿int number_01, number_02;
+
+Console.Write("Input the first number:");
+number_01 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input the first number:");
+number_02 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"{number_01} + {number_02} = " + (number_01 + number_02));
+Console.WriteLine($"{number_01} - {number_02} = " + (number_01 - number_02));
+Console.WriteLine($"{number_01} * {number_02} = " + (number_01 * number_02));
+Console.WriteLine($"{number_01} / {number_02} = " + (number_01 / number_02));
+Console.WriteLine($"{number_01} % {number_02} = " + (number_01 % number_02));
+
+
+Console.ReadLine();
