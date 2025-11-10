1794cc38ae3934a2ef1d723939b2b1fae5721b82
hasanvandi <mahdihasanvandi2540@gmail.com>
2025-11-10 12:44:34 +0330

    added MultiplyThreNumbers

---
+++ MultiplyThreeNumbers/MultiplyThreeNumbers/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Input the first number to multiply:");
+int number_01 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input the second number to multiply:");
+int number_02 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input the third number to multiply:");
+int number_03 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"{number_01}*{number_02}*{number_03} = {number_01 * number_02 * number_03}");
+
+Console.ReadLine();
