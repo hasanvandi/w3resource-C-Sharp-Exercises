f81bb2411d9f404a8bfa5f7159c18329d0aacbd3
hasanvandi <mahdihasanvandi2540@gmail.com>
2025-11-10 13:11:49 +0330

    MultiplactionTable added

---
+++ MultiplicationTable/MultiplicationTable/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Enter the number:");
+int number = int.Parse(Console.ReadLine());
+
+for(int i = 0; i <= 10; i++)
+{
+    Console.WriteLine($"{number} * {i} = " + number * i);
+}
+
+Console.ReadLine();
